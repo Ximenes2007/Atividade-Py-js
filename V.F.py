2e029be87e081605711ab3bc0021afb76973208c
ᴰʳMister2007 <lucasximenes2007@gmail.com>
2024-09-10 11:49:05 -0300

    Add files via upload

---
+++ V.F.py
@@ -0,0 +1,11 @@
+condicao1 = False
+# condicao1 = True
+
+if (condicao1):
+    print("A condição 1 é verdadeira")
+    print("Outra linha dentro da condicao 1")
+else:
+    print("A condicao 1 é falsa")
+print("Fora do if")
+
+
