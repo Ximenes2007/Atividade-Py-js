d89a57ad5a193a19b6673dc6f8ba5a38bbef05f8
ᴰʳMister2007 <lucasximenes2007@gmail.com>
2024-09-10 12:05:08 -0300

    Add files via upload

---
+++ 13.soma_de_pares.py
@@ -0,0 +1,14 @@
+numero = int(input("Digite um numero: "))
+
+# Inicialização variáveis
+soma_pares = 0
+contador = 2 # O primeiro número par
+
+# Calculando a Soma dos números pares usando o loop while
+while contadot <= numero:
+    # print(f"{soma_pares} + {contador} = {soma_pares + contador}")
+    soma_pares += contador # soma_pares = {soma_pares + contador}")
+    contador += 2 # Avançando para próximo número par
+
+# Exibindo o resultado
+print(f"A soma dos números pares até {numero} é: {soma_pares}")
